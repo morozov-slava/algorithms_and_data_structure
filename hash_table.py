02762b65da9aaa9c36c873960d7629a6217f0912
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-07-28 23:51:11 +0500

    Update hash_table.py

--- hash_table.py
+++ hash_table.py
@@ -5,24 +5,26 @@ class HashTable:
         self.slots = [None] * self.size
         
     def hash_fun(self, value):
-         index = sum([(i * ord(x))  for i, x in enumerate(value)]) % self.size
-         return index
+        index = sum([(i * ord(x))  for i, x in enumerate(value)]) % self.size
+        return index
         
     def seek_slot(self, value): 
         i = self.hash_fun(value)
+        if i < self.size:
+            return None
         if not self.slots[i] is None:
             return i
                  
     def put(self, value):
         i = self.hash_fun(value)
-        if self.slots[i] is None:
+        if i < self.size:
+            return None
+        if self.slots[i] is None):
             self.slots[i] = value
             return i
              
     def find(self, value):
         i = self.hash_fun(value)
+        if i < self.size:
+            return None
         return self.slots[i]
-
-
-
-
