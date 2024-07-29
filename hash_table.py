7b5e2f394245027e27f8e7657e6b725cb46f5e85
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-07-29 21:05:45 +0500

    Update hash_table.py

--- hash_table.py
+++ hash_table.py
@@ -3,31 +3,41 @@ class HashTable:
         self.size = sz
         self.step = stp
         self.slots = [None] * self.size
+
+    def _is_zero_size(self):
+        return self.size == 0
         
     def hash_fun(self, value):
-        index = sum([(i * ord(x))  for i, x in enumerate(value)]) % self.size
+        index = sum([(i + ord(x)) for i, x in enumerate(value)]) % self.size
         return index
         
     def seek_slot(self, value): 
-        i = self.hash_fun(value)
-        if i < self.size:
+        if self._is_zero_size():
             return None
-        if not self.slots[i] is None:
+        i = self.hash_fun(value)
+        if (i >= self.size):
+            return None
+        if self.slots[i] is None:
             return i
                  
     def put(self, value):
-        i = self.hash_fun(value)
-        if i < self.size:
+        if self._is_zero_size():
             return None
-        if self.slots[i] is None):
+        i = self.hash_fun(value)
+        if i >= self.size:
+            return None
+        if self.slots[i] is None:
             self.slots[i] = value
             return i
              
     def find(self, value):
-        i = self.hash_fun(value)
-        if i < self.size:
+        if self._is_zero_size():
             return None
-        return self.slots[i]
+        i = self.hash_fun(value)
+        if i >= self.size:
+            return None
+        if not self.slots[i] is None:
+            return i
 
 
 
