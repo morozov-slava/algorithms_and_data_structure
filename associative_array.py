50a9bbdf0020ac0a0b5d997f35d5a03fb13779a4
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-08-01 21:38:48 +0500

    Create associative_array.py

---
+++ associative_array.py
@@ -0,0 +1,25 @@
+class NativeDictionary:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+        
+    def hash_fun(self, key: str):
+        index = sum([(i + ord(x)) for i, x in enumerate(key)]) % self.size
+        return index
+        
+    def is_key(self, key: str):
+        return key in self.slots
+             
+    def put(self, key, value):
+        i = self.hash_fun(key)
+        self.slots[i] = key 
+        self.values[i] = value
+        
+    def get(self, key):
+        i = self.hash_fun(key)
+        return self.values[i]
+
+
+
+
